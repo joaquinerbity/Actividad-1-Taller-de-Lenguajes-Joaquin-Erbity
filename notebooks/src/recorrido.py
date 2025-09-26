a750c90f43e880731ba11c841e9ce270ca7b7130
joaquinerbity <joacoerbity@gmail.com>
2025-09-26 11:43:00 -0300

    Elimino modulo "recorrido", agrego comentarios al programa principal

--- notebooks/src/recorrido.py
+++
@@ -1,5 +0,0 @@
-"""
-Las funciones de este módulo son la esencia de la actividad, involucran y utilizan las funciones
-de los demás módulos para recorrer la estructura principal y devolver información de las rondas
-así como generar el acumulado para el ranking final
-"""
